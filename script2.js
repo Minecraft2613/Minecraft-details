641d7a433f565233ac791f37a667d008f7e981e5
Minecraft 2613 <1987sakshamsingh@gmail.com>
2025-06-27 19:25:35 +0530

    Update script2.js

--- script2.js
+++ script2.js
@@ -1,7 +1,6 @@
-<script>
+
   function toggleLogin() {
     const checkbox = document.getElementById('agreeCheck');
     const loginBtn = document.getElementById('loginBtn');
     loginBtn.disabled = !checkbox.checked;
   }
-</script>
